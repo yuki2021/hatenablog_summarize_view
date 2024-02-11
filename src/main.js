3bc9d8b81fed1fa370a87626c68334508a40cfc0
yuki2021 <ordinary.plusminus1@gmail.com>
2024-02-11 15:05:19 +0900

    ビルドしたファイルをアップ

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// DOMが完全に読み込まれた後に実行される関数
+function initializeVueApp() {
+    const targetElements = document.querySelectorAll('.entry-content p');
+    targetElements.forEach((elem, index) => {
+        const mountPoint = document.createElement('div');
+        mountPoint.id = `vue-app-${index}`; // 動的にIDを割り当て
+        elem.parentNode.insertBefore(mountPoint, elem);
+
+        new Vue({
+            render: h => h(App),
+        }).$mount(`#vue-app-${index}`);
+    });
+}
+
+// ページのロードが完了した後にアプリケーションを初期化
+document.addEventListener('DOMContentLoaded', initializeVueApp);
