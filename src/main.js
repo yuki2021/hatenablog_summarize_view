c278cf17c3d3bcd16713e93427daa3e021640caf
yuki2021 <ordinary.plusminus1@gmail.com>
2024-02-11 16:50:04 +0900

    再ビルド

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// DOMが完全に読み込まれた後に実行される関数
 function initializeVueApp() {
-    // '.entry-content p' の最初の要素を選択
-    const targetElement = document.querySelector('.entry-content p');
-    if (targetElement) {
-        const mountPoint = document.createElement('div');
-        // 動的にIDを割り当てる必要がない場合、単純なIDを使用
-        mountPoint.id = 'vue-app-mount-point';
-        // 最初の <p> タグの前にマウントポイントを挿入
-        targetElement.parentNode.insertBefore(mountPoint, targetElement);
+    const pElements = document.querySelectorAll('.entry-content p');
+    let inserted = false;
 
-        // Vueアプリケーションをマウント
-        createApp(App).mount('#vue-app-mount-point');
+    for (let i = 0; i < pElements.length; i++) {
+        if (pElements[i].innerText.match(/[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+/) && !inserted) {
+            const mountPoint = document.createElement('div');
+            mountPoint.id = `vue-app-mount-point`;
+            if (pElements[i].nextSibling) {
+                pElements[i].parentNode.insertBefore(mountPoint, pElements[i].nextSibling);
+            } else {
+                pElements[i].parentNode.appendChild(mountPoint);
+            }
+            createApp(App).mount(`#vue-app-mount-point`);
+            inserted = true; // マウントポイントを挿入したらループを抜ける
+            break;
+        }
     }
 }
 
-// ページのロードが完了した後にアプリケーションを初期化
 document.addEventListener('DOMContentLoaded', initializeVueApp);
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  // アウトプットディレクトリをdocに変更
   outputDir: 'docs',
-})
+  chainWebpack: config => {
+    // JavaScriptファイルのファイル名設定を変更
+    config.output
+      .filename('js/[name].js')
+      .chunkFilename('js/[name].js');
+    
+    // CSSファイルのファイル名設定を変更（Vue CLI 4以上が必要）
+    config.plugin('extract-css').tap(args => [{
+      ...args[0],
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
+    }]);
+  },
+});
